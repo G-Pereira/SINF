575d536fd77b6a64233e968820b4b5487975be34
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-03-13 20:30:30 +0000

    Readings converted to real values

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct{
   int id;
@@ -23,6 +24,7 @@ int main() {
     printf("\nNEW SENSOR DATA\n");
     token = strtok(buf, delimiter);
     int id;
+    int reading;
     for(int i = 0; token != NULL; i++ ) {
       switch(i){
         case 5:
@@ -31,37 +33,38 @@ int main() {
           devices[id-1].id = id;
           i++;
           printf("ID: %d\n", id);
-        break;
+          break;
         case 10:
-        strcat(token, strtok(NULL, delimiter));
-        devices[id-1].battery = strtol(token, NULL,16);
-        printf("Battery: %d\n", devices[id-1].battery);
-        i++;
-        break;
+          strcat(token, strtok(NULL, delimiter));
+          devices[id-1].battery = strtol(token, NULL,16)/4096*1.5;
+          printf("Battery: %d\n", devices[id-1].battery);
+          i++;
+          break;
         case 12:
-        strcat(token, strtok(NULL, delimiter));
-        devices[id-1].temperature = strtol(token, NULL,16);
-        printf("Temperature: %d\n", devices[id-1].temperature);
-        i++;
-        break;
+          strcat(token, strtok(NULL, delimiter));
+          devices[id-1].temperature = strtol(token, NULL,16)*0.01-39.6;
+          printf("Temperature: %d\n", devices[id-1].temperature);
+          i++;
+          break;
         case 14:
-        strcat(token, strtok(NULL, delimiter));
-        devices[id-1].humidity = strtol(token, NULL,16);
-        printf("Humidity: %d\n", devices[id-1].humidity);
-        i++;
-        break;
+          strcat(token, strtok(NULL, delimiter));
+          reading = strtol(token, NULL,16);
+          devices[id-1].humidity = -2.0468+0.0367*reading-1.5955*pow(10,-6)*pow(reading,2);
+          printf("Humidity: %d\n", devices[id-1].humidity);
+          i++;
+          break;
         case 16:
-        strcat(token, strtok(NULL, delimiter));
-        devices[id-1].visibleLight = strtol(token, NULL,16);
-        printf("Visible Light: %d\n", devices[id-1].visibleLight);
-        i++;
-        break;
+          strcat(token, strtok(NULL, delimiter));
+          devices[id-1].visibleLight = strtol(token, NULL,16)/4096*0.625*1.5*pow(10,6)/100;
+          printf("Visible Light: %d\n", devices[id-1].visibleLight);
+          i++;
+          break;
         case 18:
-        strcat(token, strtok(NULL, delimiter));
-        devices[id-1].infraredLight = strtol(token, NULL,16);
-        printf("Infrared Light: %d\n", devices[id-1].infraredLight);
-        i++;
-        break;
+          strcat(token, strtok(NULL, delimiter));
+          devices[id-1].infraredLight = strtol(token, NULL,16)/4096*0.769*1.5*pow(10,5)/100;
+          printf("Infrared Light: %d\n", devices[id-1].infraredLight);
+          i++;
+          break;
         }
         token = strtok(NULL, delimiter);
       }
