0ed22ddf4de016fac8658c4b917c06dc3d47e193
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-05-10 01:01:18 +0100

    prepare for auto configs change

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,29 @@
 #include "Database.h"
 #include "Data.h"
 #include "Actuator.h"
+#include "Config.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    printf("Wrong usage!\nUsage: ./a.out /dev/pts/x /dev/pts/y\n");
+    printf("Wrong usage!\nUsage: ./a.out /dev/pts/x\n");
     return 0;
   }
   printf("Welcome to HomeAuto!\nSystem Starting up...\n");
-  std::ifstream fileIn;
-  fileIn.open(argv[1]);
+  std::ifstream fileSensors;
+  fileSensors.open(argv[1]);
 
   while (1) {
     Actuator act;
     char reading[100] = "";
-    fileIn.getline(reading, 100);
+    fileSensors.getline(reading, 100);
     Data data(reading);
     data.moteId -= 1;
     act.setActuators(data);
   }
 
-  fileIn.close();
+  fileSensors.close();
   return 0;
 }
 
