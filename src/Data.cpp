650a11055c87445a0655b38bc806daef5c77f880
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-05-10 01:01:42 +0100

    fix numbers read from sensors

--- src/Data.cpp
+++ src/Data.cpp
@@ -13,11 +13,11 @@ Data::Data(std::string input) {
   boost::split(fields, input, boost::is_any_of(" "));
 
   moteId = std::stoi(fields[5] + fields[6]);
-  battery = std::stod(fields[10] + fields[11]) * 1.5 / 4096;
+  battery = (float) std::stoi(fields[10] + fields[11], 0, 16) * 1.5 / 4096;
   if (battery <= 0) return;
-  visibleLight = std::stod(fields[12] + fields[13]) * pow(10, 6) * 1.5 / 100 / 4096 * 0.625;
-  infraredLight = std::stod(fields[14] + fields[15]) * 1.5 / 100 * 0.769 / 4096;
-  temperature = std::stod(fields[16] + fields[17]) * 0.01 - 39.6;
-  humidity = -2.0468 + 0.0367 * std::stod(fields[18] + fields[19]) -
-             1.5955 * pow(10, -6) * pow(std::stod(fields[18] + fields[19]), 2);
+  visibleLight = (float) std::stoi(fields[12] + fields[13], 0, 16) * pow(10, 6) * 1.5 / 100 / 4096 * 0.625;
+  infraredLight = (float) std::stoi(fields[14] + fields[15], 0, 16) * 1.5 / 100 * 0.769 / 4096 * 1000000;
+  temperature = (float) std::stoi(fields[16] + fields[17], 0, 16) * 0.01 - 39.6;
+  humidity = -2.0468 + 0.0367 * (float) std::stoi(fields[18] + fields[19], 0, 16) -
+             1.5955 * pow(10, -6) * pow((float) std::stoi(fields[18] + fields[19], 0, 16), 2);
 }
